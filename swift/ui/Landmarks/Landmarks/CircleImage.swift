3ee49df550c13fe2ce4eed2ce588b3736c8e29cf
Tai <letientai299@gmail.com>
2021-05-09 11:11:09 +0800

    Add circular image view

---
+++ swift/ui/Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Le Tien Tai on 9/5/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("dark")
+            .frame(
+                maxWidth: 400,
+                maxHeight: 400
+            )
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
