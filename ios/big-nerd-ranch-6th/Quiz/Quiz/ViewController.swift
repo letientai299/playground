2c39708a2d1d47bb2f072837a3f9ca9d44c41340
Tai <letientai299@gmail.com>
2021-05-09 22:48:24 +0800

    Init Quiz project from the iOS 6th book

---
+++ ios/big-nerd-ranch-6th/Quiz/Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Le Tien Tai on 9/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
