2ccbe2c60675d4b4681ceab6714f973c960595e3
Tai <tai.le@shopee.com>
2021-05-09 23:43:42 +0800

    Implement logic for Quiz app

--- ios/big-nerd-ranch-6th/Quiz/Quiz/ViewController.swift
+++ ios/big-nerd-ranch-6th/Quiz/Quiz/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        questionLabel.text = questions[curQuestionId]
     }
 
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        curQuestionId += 1
+        if curQuestionId == questions.count {
+            curQuestionId = 0
+        }
 
+        let q = questions[curQuestionId]
+        questionLabel.text = q
+        answerLabel.text = "???"
+    }
+
+    @IBAction func showAnsswer(_ sender: UIButton) {
+        let a = answers[curQuestionId]
+        answerLabel.text = a
+    }
+
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+
+    var curQuestionId = 0
 }
 
