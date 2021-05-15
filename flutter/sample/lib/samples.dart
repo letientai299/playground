e9e80973767a91597317473339b0b3f266a5becd
Tai <letientai299@gmail.com>
2021-05-15 18:26:41 +0800

    Auto generate samples.dart

--- flutter/sample/lib/samples.dart
+++ flutter/sample/lib/samples.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:sample/app_1/app.dart' as app1;
-import 'package:sample/app_2/app.dart' as app2;
 import 'package:sample/startup_name/app.dart' as startupName;
+import 'package:sample/app_1/app.dart' as app1;
 
 List<Sample> samples() {
   return [
-    Sample("Startup Name", startupName.App()),
-    Sample("App 1", app1.App()),
-    Sample("App 2", app2.App()),
+    Sample('Startup Name', startupName.App()),
+    Sample('App 1', app1.App()),
   ];
 }
 
--- flutter/sample/tool/gen_main.dart
+++ flutter/sample/tool/gen_main.dart
@@ -1,1 +1,51 @@
-void main() {}
+import 'dart:io';
+
+import 'package:recase/recase.dart';
+import 'package:sprintf/sprintf.dart';
+
+const out = 'lib/samples.dart';
+const template = """
+import 'package:flutter/material.dart';
+%s
+
+List<Sample> samples() {
+  return [
+    %s
+  ];
+}
+
+class Sample {
+  final String name;
+  final Widget app;
+
+  Sample(this.name, this.app);
+}
+""";
+
+void main() {
+  var dirs = Directory('lib')
+      .listSync()
+      .where((d) => d.statSync().type == FileSystemEntityType.directory);
+  render(dirs);
+}
+
+void render(Iterable<FileSystemEntity> dirs) {
+  var imports = dirs.map((d) => toImport(d.path)).join("\n");
+  var apps = dirs.map((d) => toApp(d.path)).join("\n");
+  var code = sprintf(template, [imports, apps]);
+  File(out).writeAsString(code);
+  Process.run("dart", ["format", out]);
+}
+
+String toImport(String p) {
+  var pkg = p.replaceFirst('lib', 'sample');
+  var alias = ReCase(p.replaceFirst('lib/', '')).camelCase;
+  return "import 'package:$pkg/app.dart' as $alias;";
+}
+
+String toApp(String p) {
+  var folder = p.replaceFirst('lib/', '');
+  var alias = ReCase(folder).camelCase;
+  var name = folder.split('_').map((e) => ReCase(e).titleCase).join(" ");
+  return "Sample('$name', $alias.App()),";
+}
