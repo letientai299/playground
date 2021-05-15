1017697ef3a2423a8038abaa8309e7cdf3dc238e
Tai <letientai299@gmail.com>
2021-05-16 07:52:37 +0800

    Use parent MaterialApp for bluetooth sample

--- flutter/sample/lib/bluetooth/app.dart
+++ flutter/sample/lib/bluetooth/app.dart
@@ -8,19 +8,16 @@ import 'package:sample/bluetooth/widgets.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      color: Colors.lightBlue,
-      home: StreamBuilder<BluetoothState>(
-          stream: FlutterBlue.instance.state,
-          initialData: BluetoothState.unknown,
-          builder: (c, snapshot) {
-            final state = snapshot.data;
-            if (state == BluetoothState.on) {
-              return FindDevicesScreen();
-            }
-            return BluetoothOffScreen(state: state);
-          }),
-    );
+    return StreamBuilder<BluetoothState>(
+        stream: FlutterBlue.instance.state,
+        initialData: BluetoothState.unknown,
+        builder: (c, snapshot) {
+          final state = snapshot.data;
+          if (state == BluetoothState.on) {
+            return FindDevicesScreen();
+          }
+          return BluetoothOffScreen(state: state);
+        });
   }
 }
 
