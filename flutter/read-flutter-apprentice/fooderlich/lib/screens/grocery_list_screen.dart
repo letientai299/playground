ef295882e75615e333dcbcfe8dd2297c9d8df759
Tai <letientai299@gmail.com>
2021-08-20 16:09:35 +0700

    Make the GroceryTile updatable

--- flutter/read-flutter-apprentice/fooderlich/lib/screens/grocery_list_screen.dart
+++ flutter/read-flutter-apprentice/fooderlich/lib/screens/grocery_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fooderlich/components/grocery_tile.dart';
 import 'package:fooderlich/models/grocery_manager.dart';
+import 'package:fooderlich/screens/grocery_item_screen.dart';
 
 class GroceryListScreen extends StatelessWidget {
   final GroceryManager manager;
@@ -15,10 +16,27 @@ class GroceryListScreen extends StatelessWidget {
       child: ListView.separated(
         itemBuilder: (context, index) {
           final item = items[index];
-          return GroceryTile(
-            key: Key(item.id),
-            item: item,
-            onComplete: (change) => manager.completeItem(index, change),
+          return InkWell(
+            child: GroceryTile(
+              key: Key(item.id),
+              item: item,
+              onComplete: (change) => manager.completeItem(index, change),
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return GroceryItemScreen(
+                    originalItem: item,
+                    onCreate: (item) {},
+                    onUpdate: (item) {
+                      manager.updateItem(item, index);
+                      Navigator.pop(context);
+                    },
+                  );
+                }),
+              );
+            },
           );
         },
         separatorBuilder: (context, index) => const SizedBox(height: 16),
