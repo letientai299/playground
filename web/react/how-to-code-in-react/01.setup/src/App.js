6176f5c4617fa1e79c76e2e2e9b9a27eb9e02872
Tai <letientai299@gmail.com>
2022-05-09 01:31:49 +0700

    Dummy

---
+++ web/react/how-to-code-in-react/01.setup/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+export function App() {
+  const emojis = [
+    {
+      emo: ":smile:",
+    },
+  ];
+  return (
+    <div className={"App"}>
+      <h1>Hello</h1>
+      <p>JSX</p>
+    </div>
+  );
+}
---
+++ web/react/how-to-code-in-react/readme.md
@@ -0,0 +1,1 @@
+Following this series: https://www.digitalocean.com/community/tutorial_series/how-to-code-in-react-js
