706335d91734031ca42c531884adbcf0ca932cb3
Tai <letientai299@gmail.com>
2022-07-12 01:01:50 +0700

    Complete the Turtle Rock hard-coded Landmarks tut

--- ui/swiftui/apple-tut/Landmarks/Landmarks/ContentView.swift
+++ ui/swiftui/apple-tut/Landmarks/Landmarks/ContentView.swift
@@ -2,18 +2,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree Nation Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+                .frame(height: 300)
+
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+
+                HStack {
+                    Text("Joshua Tree Nation Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+
+
+                Divider()
+
+                Text("About Turtle Rock")
+                    .font(.title2)
+
+                Text("Description text goes here.")
+
             }
+            .padding()
+
+            Spacer()
         }
-        .padding()
     }
 }
 
