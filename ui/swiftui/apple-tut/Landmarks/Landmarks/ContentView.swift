046c049eb5b0aa5feb9ecf0e2f16e3337155d29f
Tai <letientai299@gmail.com>
2022-07-12 00:44:54 +0700

    Done section 3 building Landmarks iOS app
    https://developer.apple.com/tutorials/swiftui/creating-and-combining-views

---
+++ ui/swiftui/apple-tut/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree Nation Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ui/swiftui/apple-tut/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Le Tien Tai on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
