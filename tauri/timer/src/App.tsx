283c54b666cef05136eccfbb8226842277b435e4
Tai <letientai299@gmail.com>
2022-07-29 04:19:17 +0800

    Implement timer UI

--- tauri/timer/src/App.tsx
+++ tauri/timer/src/App.tsx
@@ -1,34 +1,163 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import parse from 'parse-duration';
+import { FaPause, FaPlay, FaUndo } from 'react-icons/fa';
 
-function App() {
-  const [count, setCount] = useState(0)
+import {
+  isPermissionGranted,
+  requestPermission,
+  sendNotification,
+} from '@tauri-apps/api/notification';
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+let permissionGranted = await isPermissionGranted();
+if (!permissionGranted) {
+  const permission = await requestPermission();
+  permissionGranted = permission === 'granted';
 }
 
-export default App
+const leftPad2 = (n: Number) => ('' + n).padStart(2, '0');
+
+function refineDuration(s: string) {
+  if (!s.endsWith('s') && !s.endsWith('h') && !s.endsWith('m')) {
+    s += 's';
+  }
+  return s;
+}
+
+const parseDuration = (s: string) => {
+  s = refineDuration(s);
+  return parse(s) / 1000;
+};
+
+function notify(duration: string) {
+  if (!permissionGranted) {
+    return;
+  }
+
+  duration = refineDuration(duration);
+  sendNotification({
+    title: 'Timer',
+    body: `Timer of ${duration} finished`,
+  });
+}
+
+function App() {
+  const [durationString, setDurationString] = useState('4s');
+  const [remain, setRemain] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    if (!running) {
+      const rem = Math.floor(parseDuration(durationString));
+      setRemain(rem);
+    }
+  }, [durationString]);
+
+  useEffect(() => {
+    if (!running) {
+      return;
+    }
+
+    const id = setInterval(() => {
+      setRemain((v) => {
+        if (v <= 0) {
+          setRunning(false);
+          notify(durationString);
+          return 0;
+        }
+        return v - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(id);
+  }, [running]);
+
+  const hour = Math.floor(remain / 3600);
+  const minute = Math.floor((remain - hour * 3600) / 60);
+  const second = remain - hour * 3600 - minute * 60;
+  const timeVisual = `${leftPad2(hour)}:${leftPad2(minute)}:${leftPad2(
+    second,
+  )}`;
+
+  let startButtonText = '';
+  if (running) {
+    startButtonText = 'Pause';
+  } else if (parseDuration(durationString) === remain) {
+    startButtonText = 'Start';
+  } else if (remain === 0) {
+    startButtonText = 'Restart';
+  } else {
+    startButtonText = 'Continue';
+  }
+
+  const start = () => {
+    if (remain === 0) {
+      reset();
+    }
+
+    setRunning(!running);
+  };
+
+  const reset = () => setRemain(parseDuration(durationString));
+
+  const onKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key != 'Enter') {
+      return;
+    }
+
+    e.preventDefault();
+    setRemain(parseDuration(durationString));
+    setRunning(true);
+  };
+
+  return (
+    <div
+      className="
+      flex gap-2 flex-col items-center justify-center w-full h-full text-2xl p-8
+    "
+    >
+      <input
+        type="text"
+        className={'text-center border-blue-200 border-2 rounded-lg w-full p-1'}
+        placeholder="Duration: 10m, 30s, ..."
+        autoFocus
+        value={durationString}
+        onChange={(e) => setDurationString(e.target.value)}
+        onKeyDown={onKeyDown}
+      />
+
+      <div className="grid grid-cols-2 w-full justify-center text-white">
+        <button
+          className={'bg-blue-500 border-2 rounded-lg p-1'}
+          onClick={start}
+        >
+          <div className="flex justify-center items-center pl-2">
+            {running ? <FaPause /> : <FaPlay />}
+            <span className={'flex-grow'}>{startButtonText}</span>
+          </div>
+        </button>
+
+        <button
+          className={'bg-orange-500 border-2 rounded-lg p-1'}
+          onClick={reset}
+        >
+          <div className="flex justify-center items-center pl-2">
+            <FaUndo />
+            <span className={'flex-grow'}>Reset</span>
+          </div>
+        </button>
+      </div>
+
+      <div
+        className="
+        grid grid-cols-8 gap-1 items-stretch
+        justify-center text-7xl font-digit text-right"
+      >
+        {[...timeVisual].map((c) => (
+          <div>{c}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
