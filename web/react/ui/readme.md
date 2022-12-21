4a1b1e357c1adc8c1139d6bab27ab24bed681f15
Tai <letientai299@gmail.com>
2022-12-21 20:00:45 +0800

    Implement basic API clients with repsonse validation

---
+++ web/react/ui/readme.md
@@ -0,0 +1,10 @@
+Might use
+
+- https://react-hook-form.com: form builder
+- https://github.com/pmndrs/zustand: state manage, alternative to redux
+- https://reactrouter.com/en/main/start/overview
+- https://github.com/fabien0102/openapi-codegen: requires OpenAPI spec.
+
+Note:
+
+- Need to batch request
--- web/react/ui/src/App.tsx
+++ web/react/ui/src/App.tsx
@@ -1,9 +1,57 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  useComment,
+  useComments,
+  usePost,
+  usePosts,
+  useProfile,
+} from "./api/Profile";
+
+export default App;
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+    },
+  },
+});
+
 function App() {
   return (
-    <div>
-      <p>Click on the Vite and React logos to learn more</p>
-    </div>
+    <QueryClientProvider client={client}>
+      <DataView header="Profile" loader={useProfile} />
+      <DataView header="Comments" loader={useComments} />
+      <DataView header="Comment 1" loader={() => useComment(1)} />
+      <DataView header="Posts" loader={usePosts} />
+      <DataView header="Post 2" loader={() => usePost(2)} />
+    </QueryClientProvider>
   );
 }
 
-export default App;
+function DataView({ header, loader }: { header: string; loader: Function }) {
+  const { status, error, data } = loader();
+  let content;
+  switch (status) {
+    case "error":
+      content = <p>{"ERR: " + error.message}</p>;
+      break;
+    case "loading":
+      content = <p>loading...</p>;
+      break;
+    case "success":
+      content = <p>{JSON.stringify(data, null, 2)}</p>;
+    default:
+      break;
+  }
+
+  return (
+    <section>
+      <h1>{header}</h1>
+      <hr />
+      <pre>
+        <code>{content}</code>
+      </pre>
+    </section>
+  );
+}
